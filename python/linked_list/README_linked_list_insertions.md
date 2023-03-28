22079caabd2e16893bb723100c05293d80c8bf02
Sheldon Pierce <Pierce55@icloud.com>
2023-03-27 20:25:46 -0700

    Completed Code Challenge for the day!

---
+++ python/linked_list/README_linked_list_insertions.md
@@ -0,0 +1,27 @@
+# Linked_List_Insertions
+<!-- Description of the challenge -->
+append
+- arguments: new value
+- adds a new node with the given value to the end of the list
+
+insert before
+- arguments: value, new value
+- adds a new node with the given new value immediately before the first node that has the value specified
+
+insert after
+- arguments: value, new value
+- adds a new node with the given new value immediately after the first node that has the value specified
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+[linked_list_insertions](./linked_list/README_linked_list_insertions.md)
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+Time - O(n)
+
+Space - O(n)
+## Solution
+<!-- Show how to run your code, and examples of it in action -->
+
+- python linked_list/linked_list.py
+
--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -21,7 +21,6 @@ class LinkedList:
             lst.append(str(None))
             return ' -> '.join(lst)
 
-
     def traverse_list(self):
         pass
 
@@ -46,25 +45,77 @@ class LinkedList:
         else:
             return False
 
+    def append(self, value):
+        current = self.head
+        node = Node(value)
+        while current.next:
+            current = current.next
+        current.next = node
+
+    def insert_before(self, value, new_value):
+        try:
+            current = self.head
+            prev = None
+            node = Node(new_value)
+            while current.value is not value:
+                prev = current
+                current = current.next
+            node.next = current
+            if prev:
+                prev.next = node
+            else:
+                self.head = node
+        except Exception:
+            raise TargetError
+
+    def insert_after(self, value, new_value):
+        try:
+            current = self.head
+            next_node = current.next
+            node = Node(new_value)
+            while current.value is not value:
+                current = current.next
+            current.next = node
+            node.next = next_node
+        except Exception:
+            raise TargetError
+
+    def delete_value(self, value):
+        current = self.head
+        if current.value == value:
+            self.head = self.head.next
+        while current.next:
+            if current.next.value == value:
+                current.next = current.next.next
+                return
+            current = current.next
+
 
 class Node:
 
     def __init__(self, value, _next=None):
         self.value = value
-        self._next = next
+        self.next = _next
 
 
-class TargetError:
+class TargetError(ValueError):
     pass
 
 
 if __name__ == "__main__":
     linked_list = LinkedList()
-    linked_list.insert(1)
-    linked_list.insert(2)
-    linked_list.insert(3)
-    linked_list.insert(4)
-    linked_list.insert(5)
+
+    linked_list.insert("apple")
+
+    linked_list.insert("banana")
+
+    linked_list.insert("cucumber")
+
+    linked_list.insert("Mango")
+
+    linked_list.insert("Watermelon")
+
+    linked_list.delete_value("cucumber")
     print(linked_list)
 
 
